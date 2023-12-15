40581a13504bdbab77cc2c77f3f375a7899d4a10
Thiago Leite <leite.tjs@gmail.com>
2023-12-15 20:07:02 -0300

    initial commit

---
+++ src/main/java/com/thiago/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,18 @@
+package com.thiago.gestao_vagas.modules.candidate.controllers;
+
+import com.thiago.gestao_vagas.modules.candidate.CandidateEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+  @PostMapping("/")
+  public void create(@RequestBody CandidateEntity candidateEntity) {
+    System.out.println("Candidato");
+    System.out.println(candidateEntity.getEmail());
+  }
+}
