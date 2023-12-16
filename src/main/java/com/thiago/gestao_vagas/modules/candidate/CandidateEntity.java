9bb7b7dbf36bebcefb79f4495d4284889c4b941e
Thiago Leite <leite.tjs@gmail.com>
2023-12-15 21:00:11 -0300

    feat: candidate validation and error handler

--- src/main/java/com/thiago/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/thiago/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,16 +1,33 @@
 package com.thiago.gestao_vagas.modules.candidate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import java.util.UUID;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class CandidateEntity {
 
   private UUID id;
   private String name;
+
+  @Pattern(
+    regexp = "\\S+",
+    message = "O campo [username] não deve conter espaços."
+  )
   private String username;
+
+  @Email(message = "O campo [email] deve conter um e-mail válido.")
   private String email;
+
+  @Length(
+    min = 5,
+    max = 10,
+    message = "A senha deve conter entre 5 e 10 caracteres."
+  )
   private String password;
+
   private String description;
   private String curriculum;
 }
